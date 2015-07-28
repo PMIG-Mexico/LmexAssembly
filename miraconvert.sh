dc3b1e902dbd9a1ef4fcf0b2f5b72c9a49dca8f0
Said Muñoz Montero <said3427@users.noreply.github.com>
2015-07-28 04:03:34 -0500

    Force non-IUPAC

---
+++ miraconvert.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Force non-IUPAC fasta results from MIRA MAF
+
+/home/said/Tools/mira_4.0.2_linux-gnu_x86_64_static/bin/miraconvert Leishmaniamexicana_out.maf LmexicanaNoIUPAC.fasta -f maf -t fasta -r C 
