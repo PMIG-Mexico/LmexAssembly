fb90458476201b46ae233096e2fb6040e82caac4
Ruben Bañuelos <krankes.hirn@gmail.com>
2015-07-28 04:01:46 -0500

    Remove IUPAC characters

--- miraconvert.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/
-#Drona
-#Force Results with non-iupac results
-/home/said/Tools/mira_4.0.2_linux-gnu_x86_64_static/bin/miraconvert Leishmaniamexicana_out.maf LmexicanaNoIUPAC.fasta -f maf -t fasta -r C 
